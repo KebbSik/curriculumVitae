0cf697f7bfee77b8fcede9bf43550805db123fb1
Kevin <kevinn.sarfoo1@gmail.com>
2025-02-28 23:52:52 +0100

    Fixed z-index of Cave section and add new images

--- cv/src/Components/Cave/Cave.tsx
+++ cv/src/Components/Cave/Cave.tsx
@@ -1,6 +1,8 @@
 import { useRef } from "react";
-import cave1 from "../../assets/cave1.png";
-import cave2 from "../../assets/cave2.jpg";
+// import cave1 from "../../assets/cave1.png";
+// import cave2 from "../../assets/cave2.jpg";
+import caveFront from "../../assets/cave_back_2.jpeg";
+import caveBack from "../../assets/cave_front_2.png";
 
 const Cave = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -23,15 +25,17 @@ const Cave = () => {
 
   return (
     <div className="whole_cave">
-      <img className="cave_img img_back" src={cave2} alt="" />
-      <img className="cave_img img_front" src={cave1} alt="" />
+      <div className="absolute_gradient"></div>
+      <div className="absolute_gradient reversed"></div>
+      <img className="cave_img img_back" src={caveFront} alt="" />
+      <img className="cave_img img_front" src={caveBack} alt="" />
 
       <section className="cave_section">
-        <button onClick={() => scrollPrev()}>
-          <i className="fa-solid fa-chevron-left"></i>
-        </button>
         <div className="cave_wrapper ">
           <div ref={containerRef} className="cave_container snap_scroller">
+            <button className="cave_btn left" onClick={() => scrollPrev()}>
+              <i className="fa-solid fa-chevron-left"></i>
+            </button>
             <div className="group">
               <div className="box"></div>
               <div className="box"></div>
@@ -55,11 +59,11 @@ const Cave = () => {
               <div className="box"></div>
               <div className="box"></div>
             </div>
+            <button className="cave_btn right" onClick={() => scrollNext()}>
+              <i className="fa-solid fa-chevron-right"></i>
+            </button>
           </div>
         </div>
-        <button onClick={() => scrollNext()}>
-          <i className="fa-solid fa-chevron-right"></i>
-        </button>
       </section>
     </div>
   );
