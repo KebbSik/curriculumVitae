13dcc6042d87a8d35cae4113d3b7dfc0eba13b31
Kevin <kevinn.sarfoo1@gmail.com>
2025-03-15 21:12:14 +0100

    Rework of skills section

--- cv/src/Components/Skills/Skills.tsx
+++ cv/src/Components/Skills/Skills.tsx
@@ -1,86 +1,60 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 
-import {
-  EffectCoverflow,
-  Pagination,
-  Autoplay,
-  FreeMode,
-  EffectFade,
-} from "swiper/modules";
 import SkillCard from "./SkillCard";
+import { motion } from "motion/react";
+import { useRef } from "react";
 
 const Skills = () => {
+  const skillList = [
+    { skillName: "HTML", percent: 77, color: "aqua" },
+    { skillName: "CSS", percent: 68, color: "yellow" },
+    { skillName: "JavaScript", percent: 83, color: "chartreuse" },
+    { skillName: "Git", percent: 57, color: "crimson" },
+    { skillName: "React", percent: 42, color: "dodgerBlue" },
+    { skillName: "Java", percent: 60, color: "lime" },
+    { skillName: "TypeScript", percent: 33, color: "orange" },
+    { skillName: "Node", percent: 45, color: "pink" },
+  ];
+
+  const dragField = useRef(null);
+
   return (
-    <section className="skills">
+    <section className="skills" ref={dragField}>
       <div className="wrapper">
-        <h2>This is couple of my skills. Feel free to check them :)</h2>
-        <div className="swiper_box">
-          <Swiper
-            effect={"coverflow"}
-            // fadeEffect={{ crossFade: true }}
-            // loopAdditionalSlides={1}
-            watchSlidesProgress={true}
-            grabCursor={true}
-            centeredSlides={true}
-            loop={true}
-            slidesPerView={"auto"}
-            // freeMode={true}
-            speed={6000}
-            // speed={0}
-            spaceBetween={50}
-            coverflowEffect={{
-              rotate: -1,
-              stretch: 20,
-              depth: 150,
-              modifier: 3,
-              scale: 0.99,
-            }}
-            // pagination={true} //add pages bar
-            autoplay={{
-              delay: 0,
-              disableOnInteraction: false,
-              pauseOnMouseEnter: true,
-            }}
-            modules={[
-              EffectCoverflow,
-              Pagination,
-              Autoplay,
-              // FreeMode,
-              EffectFade,
-            ]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <SkillCard percent={77} skillName="HMTL" color="aqua" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <SkillCard percent={68} skillName="CSS" color="yellow" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <SkillCard
-                percent={83}
-                skillName="JavaScript"
-                color="chartreuse"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <SkillCard percent={57} skillName="Git" color="crimson" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <SkillCard percent={42} skillName="React" color="dodgerBlue" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <SkillCard percent={60} skillName="Java" color="lime" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <SkillCard percent={33} skillName="TypeScript" color="orange" />
-            </SwiperSlide>
-            {/* <SwiperSlide>
-              <SkillCard percent={45} skillName="Node" color="pink" />
-            </SwiperSlide> */}
-          </Swiper>
+        <h1 style={{}}>
+          This is couple of my skills. Feel free to check them :)
+        </h1>
+        <div className="dragField">
+          {skillList.map((skill, index) => (
+            <motion.div
+              whileHover={{ scale: 1.1, cursor: "grab", zIndex: 20 }}
+              whileTap={{
+                cursor: "grabbing",
+                scale: 1.05,
+                rotate: -3,
+                filter: "drop-shadow(0 0 15px rgba(0, 0, 0, 0.5))",
+              }}
+              animate={{
+                filter: "drop-shadow(0 0 0px rgba(0, 0, 0, 0))",
+              }}
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              drag
+              dragMomentum={false}
+              dragConstraints={dragField}
+            >
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ delay: 0.5 + index * 0.2 }}
+              >
+                <SkillCard
+                  skillName={skill.skillName}
+                  percent={skill.percent}
+                  color={skill.color}
+                />
+              </motion.div>
+            </motion.div>
+          ))}
         </div>
       </div>
     </section>
