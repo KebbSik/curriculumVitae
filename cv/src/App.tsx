14c91a7218df5c458e777685a6fa8d130a4cef42
Kevin <kevinn.sarfoo1@gmail.com>
2025-03-17 23:06:19 +0100

    Added drops for background

--- cv/src/App.tsx
+++ cv/src/App.tsx
@@ -12,20 +12,36 @@ import Translator from "./Components/ColorTranslator/Translator";
 import Cave from "./Components/Cave/Cave";
 import Motion from "./Components/Motion/Motion";
 import Gradient from "./Components/Cave/Gradient";
+import { motion, useScroll, useTransform } from "motion/react";
+import waterDrops from "../src/assets/water_drops.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const { scrollYProgress } = useScroll();
+  const backGroundY = useTransform(scrollYProgress, [0, 1], ["0%", "85%"]);
   return (
     <>
+      <motion.div
+        className="drops_wrapper"
+        style={{
+          position: "absolute",
+          height: "53%",
+          width: "100%",
+          y: backGroundY,
+          backgroundImage: `url(${waterDrops})`,
+          backgroundSize: "cover",
+          opacity: 0.2,
+          zIndex: -1,
+          // overflow: "hidden",
+        }}
+      ></motion.div>
       <NavBar />
       <Paralax />
-
       <Hero />
       <About />
       <Skills />
-      {/* <Cave /> */}
-
+      <Cave />
       <Last />
       <Footer />
     </>
