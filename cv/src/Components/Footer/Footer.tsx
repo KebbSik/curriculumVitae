691b80335d34794bb720497152b4af3380927aea
Kevin <kevinn.sarfoo1@gmail.com>
2025-04-03 23:26:04 +0200

    Handle overflow on footer

--- cv/src/Components/Footer/Footer.tsx
+++ cv/src/Components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import logo from "../../assets/logo.png";
+import { HiDatabase } from "react-icons/hi";
 
 const Footer = () => {
   const whileHoverProp = {
@@ -13,38 +14,51 @@ const Footer = () => {
     transition: { duration: 0 },
   };
   return (
-    <motion.footer>
-      <motion.div
-        initial={{ y: 200, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 1, ease: "anticipate" }}
-        viewport={{ once: true }}
-        className="footer_wrapper"
-      >
-        <motion.div className="footer_left">
-          <div className="footer_logo">
-            <img src={logo} alt="logo" />
-          </div>
+    <div style={{ position: "relative", overflow: "hidden" }}>
+      <motion.footer>
+        <motion.div
+          initial={{ y: 200, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "anticipate" }}
+          viewport={{ once: true }}
+          className="footer_wrapper"
+        >
+          <motion.div className="footer_left">
+            <div className="footer_logo">
+              <img src={logo} alt="logo" />
+            </div>
+          </motion.div>
+          <motion.div className="footer_right">
+            <span>Explore me</span>
+            <ul className="footer_list">
+              <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
+                <i className="fa-brands fa-facebook"></i>
+              </motion.li>
+              <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
+                <i className="fa-brands fa-instagram"></i>
+              </motion.li>
+              <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
+                <i className="fa-brands fa-github"></i>
+              </motion.li>
+              <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
+                <i className="fa-brands fa-linkedin"></i>
+              </motion.li>
+            </ul>
+          </motion.div>
         </motion.div>
-        <motion.div className="footer_right">
-          <span>Explore me</span>
-          <ul className="footer_list">
-            <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
-              <i className="fa-brands fa-facebook"></i>
-            </motion.li>
-            <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
-              <i className="fa-brands fa-instagram"></i>
-            </motion.li>
-            <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
-              <i className="fa-brands fa-github"></i>
-            </motion.li>
-            <motion.li whileTap={whileTapProp} whileHover={whileHoverProp}>
-              <i className="fa-brands fa-linkedin"></i>
-            </motion.li>
-          </ul>
-        </motion.div>
-      </motion.div>
-    </motion.footer>
+        <div
+          className="overScroll"
+          style={{
+            position: "absolute",
+            top: "100%",
+            left: 0,
+            height: "50vh",
+            width: "100%",
+            zIndex: -3,
+          }}
+        />
+      </motion.footer>
+    </div>
   );
 };
 
