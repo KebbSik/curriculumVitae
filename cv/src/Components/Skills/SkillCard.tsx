13e17a28416b7e3e14843c25bec18ce1ec5ba89d
Kevin <kevinn.sarfoo1@gmail.com>
2025-04-05 20:23:08 +0200

    Making skillsection responsive

--- cv/src/Components/Skills/SkillCard.tsx
+++ cv/src/Components/Skills/SkillCard.tsx
@@ -1,6 +1,7 @@
 import { delay, motion, useAnimation, useInView } from "framer-motion";
 import { useEffect, useState, useRef } from "react";
 import { IconType } from "react-icons";
+import { useMediaQuery } from "react-responsive";
 
 interface Props {
   percent: number;
@@ -18,9 +19,12 @@ const outterTransiton = 1;
 const outterDelay = 0.5;
 
 const SkillCard = ({ percent, skillName, color, icon: Icon }: Props) => {
+  const max768 = useMediaQuery({ query: "(max-width: 768px)" });
+  const max480 = useMediaQuery({ query: "(max-width: 480px)" });
+
   const [percentVal, setPercentVal] = useState(0);
 
-  const controls = useAnimation();
+  // const controls = useAnimation();
   const ref = useRef(null);
   const isInView = useInView(ref, {
     margin: " -150px 0px 0px 0px",
@@ -54,33 +58,39 @@ const SkillCard = ({ percent, skillName, color, icon: Icon }: Props) => {
         >
           <Icon />
         </motion.div>
-
         <div className="percent">
-          <svg width={percentSize} height={barWidth}></svg>
+          <svg
+            width={max768 ? percentSize / 2 : percentSize}
+            height={barWidth}
+          ></svg>
           <div className="svg_lines_wrapper">
             <svg width={percentSize} height={barWidth * 2}>
               <rect
                 x="5"
                 y="1"
-                height={barWidth}
-                width={percentSize * 0.95}
+                height={max768 ? barWidth / 2 : barWidth}
+                // width={}
+                width={max768 ? (percentSize * 0.95) / 2 : percentSize * 0.95}
                 fill="#fff"
                 stroke="rgb(29, 128, 255)"
                 strokeWidth={2}
-                rx={barWidth / 2}
+                rx={max768 ? barWidth / 2 / 2 : barWidth / 2}
               />
             </svg>
-
             <motion.svg width={percentSize} height={barWidth * 2}>
               <motion.rect
                 x="5"
                 y="1"
-                height={barWidth}
+                height={max768 ? barWidth / 2 : barWidth}
                 width="0"
                 fill="rgb(29, 128, 255)"
                 strokeWidth={2}
-                rx={barWidth / 2}
-                whileInView={{ width: (percentSize * percent) / 100 }} // Directly animate width
+                rx={max768 ? barWidth / 2 / 2 : barWidth / 2}
+                whileInView={{
+                  width: max768
+                    ? (percentSize * percent) / 100 / 2
+                    : (percentSize * percent) / 100,
+                }} // Directly animate width
                 transition={{
                   duration: 2.5,
                   ease: "easeOut",
@@ -94,7 +104,6 @@ const SkillCard = ({ percent, skillName, color, icon: Icon }: Props) => {
             <sup>%</sup>
           </div>
         </div>
-
         <motion.div
           initial={{ opacity: 0, x: 50, color: "#fff" }}
           whileInView={{ opacity: 1, x: 0 }}
