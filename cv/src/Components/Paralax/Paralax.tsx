1ee7d4a5233c318e02c844cb9e7dde12d55d1a84
Kevin <kevinn.sarfoo1@gmail.com>
2025-04-05 20:40:23 +0200

    Repair scale of svg circle in paralax divider

--- cv/src/Components/Paralax/Paralax.tsx
+++ cv/src/Components/Paralax/Paralax.tsx
@@ -186,7 +186,7 @@ const Paralax = () => {
               // pathOffset: 1,
 
               fill: "rgb(3, 112, 194)",
-              scale: 3,
+              scale: max768 ? 2 : 3,
             }}
             whileInView={{ scale: 1, fill: "rgb(255, 255, 255)" }}
             viewport={{ once: true }}
