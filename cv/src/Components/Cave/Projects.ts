d794ee5579bf7ab66f009154a5dc1681d505de12
Kevin <kevinn.sarfoo1@gmail.com>
2025-06-19 20:02:12 +0200

    Added CSSPictures to projectCave

--- cv/src/Components/Cave/Projects.ts
+++ cv/src/Components/Cave/Projects.ts
@@ -1,19 +1,20 @@
 import crossedFingers from "../../assets/stickers/fingers_crossed_soon.png";
-import testImage from "../../assets/fruit_logo.png";
+import reactFruits from "../../assets/fruit_logo.png";
+import CssPictures from "../../assets/CssPictures.jpg";
 
   const projects = [
     {
       name: "SVG Fruits",
       href: "https://kebbsik.github.io/reactFruits/",
-      image: testImage,
+      image: reactFruits,
       description:
         "Playful, animated web project that showcases the appearance, properties, and global occurrence of various fruits. Each fruit is presented in a colorful, SVG-based cartoon style, blending educational content with engaging visual storytelling.",
     },
     {
-      name: "soon",
-      href: "",
-      image: crossedFingers,
-      description: "",
+      name: "CssPictures",
+      href: "https://kebbsik.github.io/CssPictures/",
+      image: CssPictures,
+      description: "Page devoted to showcase creation of graphics with CSS and HTML. Mostly inspired by CodeWar",
     },
     {
       name: "soon",
